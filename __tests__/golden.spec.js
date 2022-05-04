e82ff284fd661584a5fd8a5c850ec2b38fa26969
Emmanuel RICHE <emmanuel.riche@mysofie.fr>
2022-05-04 14:36:10 +0200

    add test to prevent regressions

---
+++ __tests__/golden.spec.js
@@ -0,0 +1,31 @@
+import fs from "fs";
+import path from "path";
+import { Drug, Pharmacy } from "../pharmacy";
+
+describe("Gloden test", () => {
+  it("output should be the same", () => {
+    const data = fs.readFileSync(
+      path.join(__dirname, "../output.txt"),
+      "utf-8"
+    );
+
+    const originalOutput = JSON.parse(`[${data}]`);
+
+    const drugs = [
+      new Drug("Doliprane", 20, 30),
+      new Drug("Herbal Tea", 10, 5),
+      new Drug("Fervex", 5, 40),
+      new Drug("Magic Pill", 15, 40)
+    ];
+
+    const trial = new Pharmacy(drugs);
+
+    const log = [];
+
+    for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
+      log.push(JSON.parse(JSON.stringify(trial.updateBenefitValue())));
+    }
+
+    expect(originalOutput).toStrictEqual(log);
+  });
+});
